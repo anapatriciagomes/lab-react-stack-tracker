fb36973879f5b3d9f5d2354d229ed5be9d7a2bee
anapatriciagomes <anapatrigomes@gmail.com>
2023-12-02 16:56:35 +0000

    code update

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,4 @@
-import { Link, useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function HomePage({ companiesData }) {
   const companies = [...companiesData].map(company => {
